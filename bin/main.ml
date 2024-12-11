e15a261d3f66dea61138cd33dc26328e8a27f67d
hyperb0rean <jewellin02@gmail.com>
2024-12-11 19:51:08 +0300

    fmt + test friendly

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,36 @@
-
 let encrypt text =
   Printf.printf "Encrypting: %s\n" text;
-  Itmo_insec.Bigram.encrypt text
+  Random.init 42;
+  Itmo_insec.Bigram.encrypt
+    text
+    (Itmo_insec.Bigram.random_square ())
+    (Itmo_insec.Bigram.random_square ())
+;;
+
 let decrypt text =
   Printf.printf "Decrypting: %s\n" text;
-  Itmo_insec.Bigram.decrypt text
+  Random.init 42;
+  Itmo_insec.Bigram.decrypt
+    text
+    (Itmo_insec.Bigram.random_square ())
+    (Itmo_insec.Bigram.random_square ())
+;;
 
 let main () =
   let mode = ref "encrypt" in
-
-  let speclist = [
-    ("-m", Arg.String (fun m -> mode := m), "Mode: 'encrypt' or 'decrypt'")
-  ] in
-
-  Arg.parse speclist (fun _ -> ()) "Usage: main -m <mode>";
-
-  let input_text = try
-    let line = read_line () in
-    if line = "" then failwith "No input provided" else line
-  with End_of_file ->
-    Printf.eprintf "Error: No input provided.\n";
-    exit 1
+  let speclist =
+    [ "-m", Arg.String (fun m -> mode := m), "Mode: 'encrypt' or 'decrypt'" ]
   in
-  
+  Arg.parse speclist (fun _ -> ()) "Usage: main -m <mode>";
+  let input_text =
+    try
+      let line = read_line () in
+      if line = "" then failwith "No input provided" else line
+    with
+    | End_of_file ->
+      Printf.eprintf "Error: No input provided.\n";
+      exit 1
+  in
   let result =
     match !mode with
     | "encrypt" -> encrypt input_text
@@ -31,7 +39,7 @@ let main () =
       Printf.eprintf "Error: Invalid mode. Use 'encrypt' or 'decrypt'.\n";
       exit 1
   in
-  
   Printf.printf "%s\n" result
+;;
 
 let () = main ()
--- lib/bigram.ml
+++ lib/bigram.ml
@@ -12,48 +12,61 @@ let shuffle arr =
     arr.(j) <- temp
   done;
   arr
+;;
 
 let random_square () =
   let chars = List.init (String.length alphabet) (fun i -> alphabet.[i]) in
   shuffle (Array.of_list chars)
+;;
 
 let bigrams text =
   let text = String.uppercase_ascii text in
   let text = String.filter (fun c -> String.contains alphabet c) text in
   let len = String.length text in
   let rec aux i acc =
-    if i >= len then List.rev acc
-    else if i = len - 1 then
-      let sym = String.sub text i 1 in aux (i + 1) ((sym ^ sym) :: acc)
-    else aux (i + 2) ((String.sub text i 2) :: acc)
-  in aux 0 []
+    if i >= len
+    then List.rev acc
+    else if i = len - 1
+    then (
+      let sym = String.sub text i 1 in
+      aux (i + 1) ((sym ^ sym) :: acc))
+    else aux (i + 2) (String.sub text i 2 :: acc)
+  in
+  aux 0 []
+;;
 
 let encrypt_bigram (b1, b2) (square1, square2) =
   let pos1 = Array.findi (fun c -> c = b1) square1 in
   let pos2 = Array.findi (fun c -> c = b2) square2 in
   let row1, col1 = pos1 / 5, pos1 mod 5 in
   let row2, col2 = pos2 / 5, pos2 mod 5 in
-  (square1.(row1 * 5 + col2), square2.(row2 * 5 + col1))
-
-let encrypt text =
-  Random.init 42;
-  let square1, square2 = random_square (), random_square () in
-  let bigrams_list = bigrams text in
-  let encrypted_bigrams = List.map (fun bg ->
-    encrypt_bigram (bg.[0], bg.[1]) (square1, square2)) bigrams_list in
-  String.concat "" (List.map (fun (a, b) -> String.make 1 a ^ String.make 1 b) encrypted_bigrams)
+  square1.((row1 * 5) + col2), square2.((row2 * 5) + col1)
+;;
 
 let decrypt_bigram (b1, b2) (square1, square2) =
   let pos1 = Array.findi (fun c -> c = b1) square2 in
   let pos2 = Array.findi (fun c -> c = b2) square1 in
   let row1, col1 = pos1 / 5, pos1 mod 5 in
   let row2, col2 = pos2 / 5, pos2 mod 5 in
-  (square2.(row2 * 5 + col1), square1.(row1 * 5 + col2))
+  square2.((row2 * 5) + col1), square1.((row1 * 5) + col2)
+;;
 
-let decrypt text =
-  Random.init 42;
-  let square1, square2 = random_square (), random_square () in
+let encrypt text square1 square2 =
   let bigrams_list = bigrams text in
-  let decrypted_bigrams = List.map (fun bg ->
-    decrypt_bigram (bg.[0], bg.[1]) (square1, square2)) bigrams_list in
-  String.concat "" (List.map (fun (a, b) -> String.make 1 a ^ String.make 1 b) decrypted_bigrams)
+  let encrypted_bigrams =
+    List.map (fun bg -> encrypt_bigram (bg.[0], bg.[1]) (square1, square2)) bigrams_list
+  in
+  String.concat
+    ""
+    (List.map (fun (a, b) -> String.make 1 a ^ String.make 1 b) encrypted_bigrams)
+;;
+
+let decrypt text square1 square2 =
+  let bigrams_list = bigrams text in
+  let decrypted_bigrams =
+    List.map (fun bg -> decrypt_bigram (bg.[0], bg.[1]) (square1, square2)) bigrams_list
+  in
+  String.concat
+    ""
+    (List.map (fun (a, b) -> String.make 1 a ^ String.make 1 b) decrypted_bigrams)
+;;
